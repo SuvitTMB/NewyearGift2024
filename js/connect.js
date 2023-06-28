1a072460f0b55d37fbaeefcd5c6f552a2cbf3122
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2023-06-28 10:58:34 +0700

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -1,13 +1,1 @@
-function Connect_DB() {
-  var firebaseConfig = {
-    apiKey: "AIzaSyDfTJJ425U4OY0xac6jdhtSxDeuJ-OF-lE",
-    authDomain: "retailproject-6f4fc.firebaseapp.com",
-    projectId: "retailproject-6f4fc",
-    databaseURL: "https://file-upload-6f4fc.firebaseio.com",
-    storageBucket: "retailproject-6f4fc.appspot.com",
-    messagingSenderId: "653667385625",
-    appId: "1:653667385625:web:a5aed08500de80839f0588",
-    measurementId: "G-9SKTRHHSW9"
-  };
-  firebase.initializeApp(firebaseConfig);
-}
+function _0x3f09(_0x44b087,_0x5f5329){var _0x4cffee=_0x4cff();return _0x3f09=function(_0x3f09c0,_0x227450){_0x3f09c0=_0x3f09c0-0x6d;var _0x4e9e6e=_0x4cffee[_0x3f09c0];return _0x4e9e6e;},_0x3f09(_0x44b087,_0x5f5329);}function _0x4cff(){var _0x40dfd6=['12NBQUwI','1:653667385625:web:a5aed08500de80839f0588','395925mkkhfI','https://file-upload-6f4fc.firebaseio.com','1931328NgSKfW','1776zuNYKr','10vdsOKk','24BQLDKV','259141mhodVY','retailproject-6f4fc.firebaseapp.com','G-9SKTRHHSW9','1240880GnKKSw','467990tuOVAn','retailproject-6f4fc.appspot.com','4980699BTWYkq'];_0x4cff=function(){return _0x40dfd6;};return _0x4cff();}(function(_0x594865,_0xdebae5){var _0x4a80af=_0x3f09,_0x3b65db=_0x594865();while(!![]){try{var _0x3c3d42=-parseInt(_0x4a80af(0x76))/0x1+-parseInt(_0x4a80af(0x7a))/0x2+parseInt(_0x4a80af(0x73))/0x3*(-parseInt(_0x4a80af(0x6e))/0x4)+-parseInt(_0x4a80af(0x70))/0x5*(parseInt(_0x4a80af(0x75))/0x6)+parseInt(_0x4a80af(0x72))/0x7+parseInt(_0x4a80af(0x79))/0x8+-parseInt(_0x4a80af(0x6d))/0x9*(-parseInt(_0x4a80af(0x74))/0xa);if(_0x3c3d42===_0xdebae5)break;else _0x3b65db['push'](_0x3b65db['shift']());}catch(_0x1632e4){_0x3b65db['push'](_0x3b65db['shift']());}}}(_0x4cff,0x2a2e5));function Connect_DB(){var _0x3ee72e=_0x3f09,_0x53311d={'apiKey':'AIzaSyDfTJJ425U4OY0xac6jdhtSxDeuJ-OF-lE','authDomain':_0x3ee72e(0x77),'projectId':'retailproject-6f4fc','databaseURL':_0x3ee72e(0x71),'storageBucket':_0x3ee72e(0x7b),'messagingSenderId':'653667385625','appId':_0x3ee72e(0x6f),'measurementId':_0x3ee72e(0x78)};firebase['initializeApp'](_0x53311d);}
